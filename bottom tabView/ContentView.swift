53f31cd24cf4b29eef5210d38944c5b5201f7c17
Onore849 <rion3376@gmail.com>
2020-08-29 15:18:25 +0900

    Initial Commit

---
+++ bottom tabView/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  bottom tabView
+//
+//  Created by 野澤拓己 on 2020/08/29.
+//  Copyright © 2020 Takumi Nozawa. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
