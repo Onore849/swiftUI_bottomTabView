07e9c904bf53c88148f7b00e8938551bea70a68d
Onore849 <rion3376@gmail.com>
2020-08-29 15:31:21 +0900

    create Tabbed View

--- bottom tabView/ContentView.swift
+++ bottom tabView/ContentView.swift
@@ -10,7 +10,41 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        
+        TabView{
+            
+            page1().tabItem {
+                
+                VStack {
+                    Image(systemName: "house.fill")
+                    Text("Home").fontWeight(.light)
+                }
+                
+            }
+            
+            page2().tabItem {
+                
+                VStack {
+                    
+                    Image(systemName: "flame.fill")
+                    Text("News").fontWeight(.light)
+                    
+                }
+                
+            }
+            
+            page3().tabItem {
+                
+                VStack {
+                    
+                    Image(systemName: "paperplane.fill")
+                    Text("Share").fontWeight(.light)
+    
+                }
+            }
+            
+        }
+        
     }
 }
 
@@ -18,4 +52,34 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
+
+struct page1: View {
+    
+    var body: some View{
+        
+        Text("Hello from page 1").fontWeight(.heavy)
+        
+    }
+    
+}
+
+struct page2: View {
+    
+    var body: some View{
+        
+        Text("Hello from page 2").fontWeight(.heavy)
+        
+    }
+    
+}
+
+struct page3: View {
+    
+    var body: some View{
+        
+        Text("Hello from page 3").fontWeight(.heavy)
+        
+    }
+    
 }
